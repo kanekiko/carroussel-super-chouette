65f5ab2881476bf7324626c96c2a49f630ddb44f
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-23 01:53:02 +0200

    feat: add carousel script

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,1 +1,15 @@
 "use strict"
+
+window.addEventListener('load', function () {
+  let arrowLeft = document.getElementById('carousel-left');
+  let arrowRight = document.getElementById('carousel-right');
+
+  arrowRight.addEventListener('click', function () {
+    document.getElementById('scroll').scrollLeft += 2000;
+  })
+
+  arrowLeft.addEventListener('click', function () {
+    document.getElementById('scroll').scrollLeft -= 2000;
+  })
+
+})
